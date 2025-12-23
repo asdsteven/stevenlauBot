10481f916076b5828873c881bfd0ef0247125e8f
s <s@macbook-air-2025.local>
2025-12-23 10:44:14 +0800

    use teleport

--- simulation/ai/stevenlau/_stevenlaubot.js
+++ simulation/ai/stevenlau/_stevenlaubot.js
@@ -14,33 +14,38 @@ STEVENLAU.StevenlauBot.prototype.CustomInit = function(gameState)
     this.state = 0;
 };
 
-STEVENLAU.StevenlauBot.prototype.countGenericNames = function() {
+STEVENLAU.StevenlauBot.prototype.printGenericNames = function() {
     const genericNames = new Map();
     this.gameState.getEntities().forEach(ent => {
         const name = ent.genericName();
         genericNames.set(name, (genericNames.get(name) || 0) + 1);
     });
-    for (const [name, count] of genericNames) {
-        this.chat(`${name}: ${count}`);
-    }
+    genericNames.forEach(([name, count]) => this.chat(`${name}: ${count}`));
+};
+
+function roundDD(x) {
+    return Math.round(x * 100) / 100;
+}
+
+function positionToString([x, z]) {
+    return `(${roundDD(x)},${roundDD(z)})`;
 }
 
 function pointSegmentDistanceSquared(target, p1, p2) {
     const u = new Vector2D(target[0] - p1[0], target[1] - p1[1]);
     const v = new Vector2D(p2[0] - p1[0], p2[1] - p1[1]);
     const t = u.dot(v) / v.dot(v);
-    if (t <= 0) {
-        return API3.SquareVectorDistance(target, p1);
-    }
-    if (1 <= t) {
-        return API3.SquareVectorDistance(target, p2);
-    }
+    if (t <= 0) return API3.SquareVectorDistance(target, p1);
+    if (1 <= t) return API3.SquareVectorDistance(target, p2);
     return v.mult(t).add(new Vector2D(p1[0], p1[1])).distanceToSquared(new Vector2D(target[0], target[1]));
 }
 
-function structureCorners([x, z], angle, w, h) {
-    const cosa = Math.cos(angle);
-    const sina = Math.sin(angle);
+function structureCorners(structure) {
+    const [x, z] = structure.position();
+    const w = +structure.get("Obstruction/Static/@width");
+    const h = +structure.get("Obstruction/Static/@depth");
+    const cosa = Math.cos(structure.angle());
+    const sina = Math.sin(structure.angle());
     const dxz = [[h,w],[-h,w],[-h,-w],[h,-w],[h,w]];
     return [0,1,2,3].map(i => {
         const [dx1, dz1] = dxz[i];
@@ -57,69 +62,56 @@ function structureCorners([x, z], angle, w, h) {
     });
 }
 
-STEVENLAU.StevenlauBot.prototype.firstWoodFruitChicken = function() {
-    let cc;
-    this.gameState.getOwnDropsites().forEach(site => cc = site);
-    this.chat(`cc: ${cc.position()} ${cc.angle()}`);
-    const ccCorners = structureCorners(cc.position(), cc.angle(), +cc.get("Obstruction/Static/@width"), +cc.get("Obstruction/Static/@depth"));
+function pointStructureDistanceSquared(target, structure) {
+    return Math.min(...structureCorners(structure).map(([p1,p2]) => {
+        return pointSegmentDistanceSquared(target, p1, p2);
+    }));
+}
 
-    let cav;
-    this.gameState.getOwnUnits().forEach(unit => {
-        if (unit.genericName() == "Cavalry Swordsman") {
-            cav = unit;
-        }
+STEVENLAU.StevenlauBot.prototype.firstEntities = function() {
+    const entities = {
+        women: [],
+        crossbowmen: [],
+        spearmen: [],
+        swordcav: null,
+        minister: null,
+        cc: null
+    };
+    this.gameState.getOwnEntities().forEach(ent => {
+        const s = ent.genericName();
+        if (s == "Female Citizen") entities.women.push(ent);
+        else if (s == "Infantry Crossbowman") entities.crossbowmen.push(ent);
+        else if (s == "Spearman") entities.spearmen.push(ent);
+        else if (s == "Cavalry Swordsman") entities.swordcav = ent;
+        else if (s == "Imperial Minister") entities.minister = ent;
+        else if (s == "Civic Center") entities.cc = ent;
+        else this.chat(`unhandled first entity: ${s}`);
     });
+    this.chat(`cc: ${positionToString(entities.cc.position())} ${roundDD(entities.cc.angle())}rad`);
+    return entities;
+};
 
-    const list = [];
-    this.gameState.getResourceSupplies("wood").forEach(supply => {
-        list.push([supply, API3.SquareVectorDistance(supply.position(), cc.position())]);
-    });
-    list.sort((a, b) => a[1] - b[1]);
-    list.slice(0, 10).forEach(a => this.chat(`tree: ${a[0].position()}: ${a[1]}`));
-
-    const nearestSupply = new Map();
+STEVENLAU.StevenlauBot.prototype.dropsiteTreeFruitMeat = function(dropsite) {
+    const m = new Map();
     const f = supply => {
         const res = supply.resourceSupplyType().specific;
-        let dist;
-        if (res == "meat") {
-            dist = API3.SquareVectorDistance(supply.position(), cav.position())
-        } else {
-            dist = Math.min(...ccCorners.map(([p1,p2]) => {
-                return pointSegmentDistanceSquared(supply.position(), p1, p2);
-            }));
-        }
-        if (!nearestSupply.has(res)) {
-            nearestSupply.set(res, [supply, dist]);
-        } else if (dist < nearestSupply.get(res)[1]) {
-            nearestSupply.set(res, [supply, dist]);
-            /* this.chat(`${supply.position()}: ${dist}`); */
-        }
+        const dist = pointStructureDistanceSquared(supply.position(), dropsite);
+        if (!m.has(res) || dist < m.get(res)[1]) m.set(res, [supply, dist]);
     };
     this.gameState.getResourceSupplies("wood").forEach(f);
     this.gameState.getResourceSupplies("food").forEach(f);
-    this.chat(`nearest: ${nearestSupply.get("tree")[0].position()}, ${nearestSupply.get("tree")[1]}`);
-    return ["tree","fruit","meat"].map(res => nearestSupply.get(res)[0]);
+    return ["tree", "fruit", "meat"].map(res => m.get(res)[0]);
 }
 
-STEVENLAU.StevenlauBot.prototype.nearest = function(wood, tree) {
-    let cc;
-    this.gameState.getOwnDropsites().forEach(site => cc = site);
-    const ccCorners = structureCorners(cc.position(), cc.angle(), +cc.get("Obstruction/Static/@width"), +cc.get("Obstruction/Static/@depth"));
-
-    const nearestSupply = new Map();
+STEVENLAU.StevenlauBot.prototype.cavMeat = function(cav) {
+    let meat = null;
     const f = supply => {
-        const res = supply.resourceSupplyType().specific;
-        const dist = Math.min(...ccCorners.map(([p1,p2]) => {
-            return pointSegmentDistanceSquared(supply.position(), p1, p2);
-        }));
-        if (!nearestSupply.has(res)) {
-            nearestSupply.set(res, [supply, dist]);
-        } else if (dist < nearestSupply.get(res)[1]) {
-            nearestSupply.set(res, [supply, dist]);
-        }
+        if (supply.resourceSupplyType().specific != "meat") return;
+        const dist = API3.SquareVectorDistance(supply.position(), cav.position())
+        if (!meat || dist < meat[1]) meat = [supply, dist];
     };
-    this.gameState.getResourceSupplies(wood).forEach(f);
-    return nearestSupply.get(tree)[0];
+    this.gameState.getResourceSupplies("food").forEach(f);
+    return meat[0];
 }
 
 STEVENLAU.StevenlauBot.prototype.OnUpdate = function(gameState)
@@ -130,45 +122,39 @@ STEVENLAU.StevenlauBot.prototype.OnUpdate = function(gameState)
             this.state = -1;
             return;
         }
-
-        const [wood,fruit,chicken] = this.firstWoodFruitChicken();
-        this.gameState.getOwnUnits().forEach(unit => {
-            if (!unit.isIdle()) {
-                return;
-            }
-            if (unit.genericName() == "Infantry Crossbowman") {
-                unit.gather(wood);
-            } else if (unit.genericName() == "Spearman") {
-                unit.gather(wood);
-            } else if (unit.genericName() == "Female Citizen") {
-                unit.gather(wood);
-            } else if (unit.genericName() == "Cavalry Swordsman") {
-                unit.gather(chicken);
-            } else if (unit.genericName() == "Imperial Minister") {
+        this.entities = this.firstEntities();
+        this.teleporting = new Map();
+        const {women, crossbowmen, spearmen, swordcav, minister, cc} = this.entities;
+        const [tree, fruit, ] = this.dropsiteTreeFruitMeat(cc);
+        const chicken = this.cavMeat(swordcav);
+        const unitTargets = [[swordcav, chicken]];
+        [...women, ...crossbowmen, ...spearmen, minister].forEach(unit => unitTargets.push([unit, tree]));
+        for (const [unit, target] of unitTargets) {
+            if (API3.SquareVectorDistance(target.position(), unit.position()) < pointStructureDistanceSquared(target.position(), cc)) {
+                unit.gather(target);
             } else {
-                this.chat(`unhandled unit: ${unit.genericName()}`);
+                unit.garrison(cc);
+                this.teleporting.set(unit.id(), [unit, target]);
             }
-        });
-
+        }
         this.state = 1;
     } else if (this.state == 1) {
-        this.gameState.getOwnUnits().forEach(unit => {
-            if (!unit.isIdle()) {
-                return;
+        const {women, crossbowmen, spearmen, swordcav, minister, cc} = this.entities;
+        for (const id of cc.garrisoned()) {
+            if (!this.teleporting.has(id)) {
+                this.chat(`unknown garrison: ${id}`);
+                continue;
             }
-            if (unit.genericName() == "Infantry Crossbowman") {
-                unit.gather(this.nearest("wood", "tree"));
-            } else if (unit.genericName() == "Spearman") {
-                unit.gather(this.nearest("wood", "tree"));
-            } else if (unit.genericName() == "Female Citizen") {
-                unit.gather(this.nearest("wood", "tree"));
-            } else if (unit.genericName() == "Cavalry Swordsman") {
-                unit.gather(this.nearest("food", "meat"));
-            } else if (unit.genericName() == "Imperial Minister") {
-            } else {
-                this.chat(`unhandled unit: ${unit.genericName()}`);
-            }
-        });
+            const [unit, target] = this.teleporting.get(id);
+            cc.setRallyPoint(target, "gather");
+            cc.unload(id);
+            this.teleporting.delete(id);
+        }
+        if (this.teleporting.size == 0) this.state = 2;
+    } else if (this.state == 2) {
+        const {women, crossbowmen, spearmen, swordcav, minister, cc} = this.entities;
+        const [ , , meat] = this.dropsiteTreeFruitMeat(cc);
+        if (swordcav.isIdle()) swordcav.gather(meat);
     }
 };
 
